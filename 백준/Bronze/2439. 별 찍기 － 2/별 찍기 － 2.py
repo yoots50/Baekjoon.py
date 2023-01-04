deaa0edd8e3a5ed11bbcaa10dfb61335fec5424b
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-01-04 23:22:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 40 ms, Memory: 30644 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,9 @@
+def main():
+    N = int(input())
+    for i in range(N):
+        for j in range(N - i - 1):
+            print(" ", end = "")
+        for j in range(i + 1):
+            print("*", end = "")
+        print("")
+main()
