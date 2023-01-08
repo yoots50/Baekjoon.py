d6f21104ff16cc0b940f05c14e72b3131ef25ce8
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-01-08 23:05:10 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,10 @@
+def main():
+    A, B = map(int, input().split())
+    A = int(str(A)[::-1])
+    B = int(str(B)[::-1])
+    if (A > B):
+        print(A)
+    else:
+        print(B)
+        
+main()
