de5d4e3e59e8eb0c56508e22134b05e22cdc9a31
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-01-08 23:36:13 +0900

    [Bronze II] Title: 단어의 개수, Time: 56 ms, Memory: 38292 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,14 +1,5 @@
 def main():
-    string = input().strip()
-    if string == '':
-        print(0)
-        return 0
-    cnt = 1
-    while True:
-        if string.find(' ') + 1:
-            string = string[string.find(' ') + 1:]
-            cnt += 1
-        else:
-            print(cnt)
-            return 0
+    arr = input().split()
+    print(len(arr))
+
 main()
