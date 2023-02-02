0f67d8f45ce2777f6df6eaf2039b0bab9a6da474
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-02-02 20:01:16 +0900

    [Bronze V] Title: Serca, Time: 444 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,11 @@
+n = int(input())
+for _ in range(n):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
