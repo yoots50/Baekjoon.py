2f17b0724ba7fdf129813d292e571e7bdef44463
yoots50 <202210947@sangmyung.kr>
2025-12-31 11:26:47 +0900

    [Silver III] Title: 2×n 타일링, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,17 @@
+def main():
+    n = int(input())
+    if n == 1:
+        print(1)
+    elif n == 2:
+        print(2)
+    else:
+        memo = [-1] * (n + 1)
+        memo[1] = 1
+        memo[2] = 2
+        for i in range(3, n + 1):
+            memo[i] = memo[i - 1] + memo[i - 2]
+        print(memo[n] % 10007)
+
+
+if __name__ == "__main__":
+    main()
